f8cf97095264a65cbb8303eab545a5a3f454215d
manish-giri <manish.giri.me@gmail.com>
2016-06-23 17:58:27 -0400

    Use node-persist to save a variable

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,1 +1,8 @@
 console.log("Starting password manager");
+
+var storage = require('node-persist');
+storage.initSync();
+
+//storage.setItemSync('name', 'Manish');
+var userName = storage.getItemSync('name');
+console.log("Saved name is = " + userName);
