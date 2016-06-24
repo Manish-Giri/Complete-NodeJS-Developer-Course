caab7d3d4095475846393cbfd7b68f459ab87844
manish-giri <manish.giri.me@gmail.com>
2016-06-24 12:37:53 -0400

    Save and fetch multiple accounts

--- password-manager/app.js
+++ password-manager/app.js
@@ -4,12 +4,17 @@ var storage = require('node-persist');
 storage.initSync();
 
 //storage.setItemSync('name', 'Manish');
- storage.setItemSync('accounts', [
+ /*storage.setItemSync('accounts', [
    {username: "Andrew", balance: 0}
- ]);
+ ]);*/
 
-var userName = storage.getItemSync('name');
-console.log("Saved name is = " + userName);
+// var userName = storage.getItemSync('name');
+// console.log("Saved name is = " + userName);
 
-var account = storage.getItemSync('accounts');
-console.log("Accounts = " + account);
+// var account_array = storage.getItemSync('accounts');
+// console.log("Accounts = " + account_array);
+// account_array.push({username: "Jen", balance: 20});
+//
+// storage.setItemSync('accounts', account_array);
+
+console.log(storage.getItemSync('accounts'));
