c8b1f2f9e5e8431e242a0caa4ce3e0f8ebbb4a5b
manish-giri <manish.giri.me@gmail.com>
2016-06-24 12:30:16 -0400

    Fetch an array of accounts

--- password-manager/app.js
+++ password-manager/app.js
@@ -4,5 +4,12 @@ var storage = require('node-persist');
 storage.initSync();
 
 //storage.setItemSync('name', 'Manish');
+ storage.setItemSync('accounts', [
+   {username: "Andrew", balance: 0}
+ ]);
+
 var userName = storage.getItemSync('name');
 console.log("Saved name is = " + userName);
+
+var account = storage.getItemSync('accounts');
+console.log("Accounts = " + account);
