4dcd590f8181ae18311b6070ae103401f53c88bf
manish-giri <manish.giri.me@gmail.com>
2016-06-28 19:57:01 -0400

    Add implementation of getAccount, remove earlier persist variables

--- password-manager/app.js
+++ password-manager/app.js
@@ -6,8 +6,31 @@ storage.initSync();
 //function to create an account, arguments - account object
 //account object - name(facebook), username, password
 function createAccount(account) {
-  
+  //first, get accounts stored with node-persist
+  var accounts = storage.getItemSync("accounts");
+
+  //if returned item is undefined, set it to an empty array
+  if(typeof accounts === "undefined") {
+    accounts = [];
+    accounts.push(account);
+  }
+
+  return account;
 }
 
 //function to get an account - arguments - account name
-function getAccount(accountName) {}
+function getAccount(accountName) {
+  //load all saved accounts
+  var accounts = storage.getItemSync("accounts");
+  var matchedAccount = {};
+  accounts.forEach(function(account) {
+    if(account.name === accountName) {
+      matchedAccount = account;
+    }
+  });
+  return matchedAccount;
+}
+
+createAccount({
+  name: "Facebook", username: "randomname", password: "Password!"
+});
