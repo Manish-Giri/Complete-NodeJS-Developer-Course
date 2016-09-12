af62dde1c7dbe110e9aaf48df46c3e675fbbd4ea
MANISH-GIRI <manish.giri.me@gmail.com>
2016-09-11 23:25:09 -0400

    Get input from user with argv

--- password-manager/example-args.js
+++ password-manager/example-args.js
@@ -1,6 +1,15 @@
 var argv = require('yargs').argv;
-
+var command = argv._[0];
 console.log(argv);
-if(argv._[0] === 'hello') {
-    console.log("Hello World");
+//
+// if(argv._[0] === 'hello') {
+//     console.log("Hello World");
+// }
+
+if(command === 'hello' && typeof argv.name !== 'undefined') {
+    console.log("Hello, " + argv.name + "!");
+}
+
+else if(command === 'hello') {
+    console.log("Hello World!");
 }
