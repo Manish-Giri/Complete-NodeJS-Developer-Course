f2c1e8ea1f2579f026b67cb9efc55b60cd6440bc
Manish Giri <manish.giri.me@gmail.com>
2016-10-22 18:04:12 -0400

    Convert JSON to JS object and back

--- password-manager/example-json.js
+++ password-manager/example-json.js
@@ -15,3 +15,14 @@ console.log();
 var personObject = JSON.parse(personJSON);
 console.log(`personObject.name = ${personObject.name}`);
 console.log(typeof personObject);
+
+//challenge
+var animal = '{"name": "Halley"}';
+
+//convert string to JS object, add an age property, convert back 
+var animalObject = JSON.parse(animal);
+console.log(`animalObject.name = ${animalObject.name}`);
+animalObject.age = 4;
+console.log(`animalObject.age = ${animalObject.age}`);
+var animalJSON = JSON.stringify(animalObject)
+console.log(`animal object JSON = ${animalJSON}`);
