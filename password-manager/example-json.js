5f687982ef1810f4faaec881a3072f7d3c7bd3b6
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-22 15:19:33 -0400

    Convert JS object to JSON with stringify()

--- password-manager/example-json.js
+++ password-manager/example-json.js
@@ -1,3 +1,12 @@
 /**
  * Created by manishgiri on 10/22/16.
  */
+var person = {
+    name: 'Andrew',
+    age: 24
+}
+
+var personJSON = JSON.stringify(person);
+console.log(`personJSON = ${personJSON} `);
+console.log(typeof personJSON);
+
