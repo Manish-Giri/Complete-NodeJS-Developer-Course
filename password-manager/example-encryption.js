ea72f9b205bb2fac8cd0354032c4445adb60d76b
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-23 01:41:58 -0400

    Encrypt a JS object, then decrypt it, using crypto module

--- password-manager/example-encryption.js
+++ password-manager/example-encryption.js
@@ -4,12 +4,22 @@
 var crypto = require('crypto-js');
 
 //message
-var secretMessage = "I hid the chips under the couch.";
+//var secretMessage = "I hid the chips under the couch.";
+var secretMessage = {
+    name: "Andrew",
+    code: 007
+};
+
 //key
 var secretKey = '123abc';
 
 //encrypt the message
-var encryptedMessage = crypto.AES.encrypt(secretMessage, secretKey);
+//encrypt() takes a string, so use JSON.stringify to create JSON string
+var secretMessageJSON = JSON.stringify(secretMessage);
+console.log(`Secret Message in JSON: ${secretMessageJSON}`);
+
+//var encryptedMessage = crypto.AES.encrypt(secretMessage, secretKey);
+var encryptedMessage = crypto.AES.encrypt(secretMessageJSON, secretKey);
 console.log(`Encrypted Message: ${encryptedMessage}`);
 
 //decrypt the message
@@ -17,3 +27,10 @@ var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
 //convert array of bytes to string
 var decryptedMessage = bytes.toString(crypto.enc.Utf8);
 console.log(`Decrypted Message: ${decryptedMessage}`);
+
+//convert decrypted string message back to JS object
+var decryptedMessageObject = JSON.parse(decryptedMessage);
+console.log(`Type of decrypted message object: ${typeof decryptedMessageObject}`);
+console.log(`object.name = ${decryptedMessageObject.name}`);
+
+//challenge - encrypt an object, instead of a string
