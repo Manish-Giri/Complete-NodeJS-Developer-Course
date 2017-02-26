82824ac245393b9d97ba32054d8630b41d2eeb45
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-25 21:28:12 -0500

    Use a new file 'app.js' to require 'weather' and use it

---
+++ async/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by manishgiri on 2/25/17.
+ */
+var weather = require('./weather.js');
+
+weather();
--- async/weather.js
+++ async/weather.js
@@ -6,19 +6,31 @@ var request = require('request');
 //var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=72c269f82f1a47216a1106cd04e0f37a";
 var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ae0acb60e8db4952e081c2fb470a1b23";
 
-request({url: 'http://ipinfo.io', json: true}, function(error, res, body) {
-    console.log(JSON.stringify(body, null, 4));
-});
+function weather() {
 
-request({
-    url: url,
-    json: true,
+
+    request({url: 'http://ipinfo.io', json: true}, function (error, res, body) {
+        if (error) {
+            console.log("Unable to fetch weather");
+        }
+        else {
+            console.log(JSON.stringify(body, null, 4));
+        }
+
+    });
+
+    request({
+        url: url,
+        json: true,
 
     }, function (error, response, body) {
-    if(error) {
-        console.log("Unable to fetch weather");
-    }
-    else {
-        console.log(JSON.stringify(body, null, 4));
-    }
-});
+        if (error) {
+            console.log("Unable to fetch weather");
+        }
+        else {
+            console.log(JSON.stringify(body, null, 4));
+        }
+    });
+}
+
+module.exports = weather;
