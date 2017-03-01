a15141747583df0fd561e45604c037efff22b866
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-28 23:13:21 -0500

    Redo app.js using Promises

---
+++ async/app-prom.js
@@ -0,0 +1,40 @@
+/**
+ * Created by manishgiri on 2/28/17.
+ */
+var weather = require('./weather-prom.js');
+var location = require('./location-prom');
+
+//use yargs to get user's input
+var argv = require('yargs').option("location", {
+    alias: "l",
+    demand: false,
+    describe: "Location to fetch weather for",
+    type: "string"
+}).help("help").argv;
+
+if(typeof argv.l === "string" && argv.l.length > 0) {
+    //user has entered specific location - call weather with this location
+    weather(argv.l).then(function (res) {
+        var temp = res.main.temp;
+        var city = res.name;
+        return {temp, city};
+        //console.log(res);
+    }).then(function (resObj) {
+        console.log(`It's ${resObj.temp} degrees Celsius in ${resObj.city}`);
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+else {
+    //call weather with user's default location
+    //var lat,lon,city;
+    location(function (result) {
+        var coords = result.loc.split(",");
+        var lat = coords[0];
+        var lon = coords[1];
+        var city = result.city;
+        console.log(city);
+        weather({lat, lon, city});
+    });
+}
