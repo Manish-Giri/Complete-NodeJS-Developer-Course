1c19f5898fd23fec91301653424feac61e568935
MANISH-GIRI <manish.giri.me@gmail.com>
2017-04-09 01:10:33 -0400

    Add a logger to log request data

--- web-server/server.js
+++ web-server/server.js
@@ -15,12 +15,18 @@ var middleware = {
     requireAuthentication: function (req, res, next) {
         console.log("Private route hit");
         next();
+    },
+    logger: function (req, res, next) {
+        var date = new Date().toDateString();
+        console.log("Request: " + req.method + " " + req.originalUrl + " at " + date);
+        next();
     }
 };
 
 
 //application level middleware
 //app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
 
 app.get("/about", middleware.requireAuthentication, function (req, res) {
     res.send("About Me");
