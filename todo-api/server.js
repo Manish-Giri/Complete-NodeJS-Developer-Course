0a239016a9049ac0a99f0586fbdd615d5c99d152
MANISH-GIRI <manish.giri.me@gmail.com>
2017-04-11 20:22:12 -0400

    Fetch a TODO item with an ID

--- todo-api/server.js
+++ todo-api/server.js
@@ -16,7 +16,13 @@ var todos = [
         id: 2,
         description: "go to market",
         completed: false
+    },
+    {
+        id: 3,
+        description: "Learn node",
+        completed: true
     }
+
 ]
 
 /**
@@ -29,6 +35,24 @@ app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
+app.get('/todos/:id', function (req, res) {
+    var todoID = parseInt(req.params.id);
+    var todoItem = {};
+    todos.forEach(function (todo) {
+        if(todoID === todo.id) {
+            todoItem = todo;
+        }
+    })
+    var isObjEmpty = !Object.keys(todoItem).length;
+
+    if(isObjEmpty) {
+        res.status(404).send();
+    }
+    else {
+        res.json(todoItem);
+    }
+    //res.send("Asking for todo with ID " + req.params.id);
+})
 
 app.get('/', function (req, res) {
     res.send("Todo API Root");
