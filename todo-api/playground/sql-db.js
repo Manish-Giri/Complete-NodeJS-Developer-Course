8a8557c341d229d71ce832f9a9459c70ec2f22dd
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-02 23:33:09 -0400

    Define schema for the Todo/Todos model, insert a record in the table

--- todo-api/playground/sql-db.js
+++ todo-api/playground/sql-db.js
@@ -8,7 +8,24 @@ const sequelize = new Sequelize(undefined, undefined, undefined, {
     'storage': __dirname + '/sql-db.sqlite'
 });
 
+let Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING
+    },
+    completed: {
+        type: Sequelize.BOOLEAN
+    }
+});
+
 sequelize.sync().then(function () {
     console.log("Everything is synced.");
-})
+
+    Todo.create({
+        description: "Walk the dog",
+        completed: false
+    }).then(function (todo) {
+        console.log("Finished");
+        console.log(todo);
+    })
+});
 
