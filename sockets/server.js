6a3103e8da3778ffd1f1a40004cfab7d2f379aa9
Manish Giri <manish.giri.me@gmail.com>
2018-02-17 14:27:14 -0500

    Create connection event, add front end for app

--- sockets/server.js
+++ sockets/server.js
@@ -1,8 +1,20 @@
+// imports
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
+// TODO: read docs on these statements
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', () => console.log("User connected via Socket.io!"));
+
+
+
+
+
+
+
 
 http.listen(PORT, () => console.log("Server started"));
