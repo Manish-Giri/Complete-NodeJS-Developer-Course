b55c1005fd15f28f31e23f542ea2a161bd3c20f9
Manish Giri <manish.giri.me@gmail.com>
2018-02-17 19:54:43 -0500

    Setup object to keep track of each distinct socket, add disconnect feature

--- sockets/server.js
+++ sockets/server.js
@@ -9,6 +9,8 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+let clientInfo = {};
+
 io.on('connection', (socket) => {
     console.log("User connected via Socket.io!");
     console.log("------");
@@ -22,6 +24,24 @@ io.on('connection', (socket) => {
         at: moment().valueOf()
     });
 
+    // listen for join room event
+    socket.on('joinRoom', req => {
+
+        // set room info for use later (broadcast only to this room)
+        clientInfo[socket.id] = req;
+
+        // attach this socket to the specified room
+        socket.join(req.room);
+
+        // inform other users of THIS ROOM of new user joining
+        socket.broadcast.to(req.room).emit('message', {
+            name: "The Matrix",
+            text: `${req.name} has joined the room`,
+            at: moment.valueOf()
+        });
+
+    });
+
     // listen for custom message from one socket and broadcast to all other sockets
     socket.on('message', message => {
         console.log(`Message received: ${message.text}`);
@@ -32,8 +52,27 @@ io.on('connection', (socket) => {
         // SECOND user message
         // broadcast to everyone but sender
         // NOTE: use io.emit() for sending to sender too
-        io.emit('message', message);
 
+        // send message only to a specific room (get ID from info set earlier while joining room)
+        io.to(clientInfo[socket.id].room).emit('message', message);
+
+    });
+
+
+    // listen for socket disconnection event and exit user from room, if user exists
+    socket.on('disconnect', () => {
+        const userData = clientInfo[socket.id];
+        if(userData) {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+                name: "The Matrix",
+                text: `${userData.name} has left the room`,
+                at: moment.valueOf()
+            });
+
+            // delete this user's data
+            delete clientInfo[socket.id];
+        }
     });
 
 });
